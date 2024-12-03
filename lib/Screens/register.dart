bf19b952a4968a1472c453114a2868d133bafd30
DiantoroKadarman <diantorokadarman03@gmail.com>
2024-12-03 22:50:55 +0700

    register

--- lib/Screens/register.dart
+++ lib/Screens/register.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:leafyfun/Screens/login.dart';
 
@@ -9,6 +11,9 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool _obscureText = true;
 
   void _togglePasswordVisibility() {
@@ -17,29 +22,59 @@ class _RegisterScreenState extends State<RegisterScreen> {
     });
   }
 
+  Future<void> _register() async {
+    final username = _usernameController.text;
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://127.0.0.1:8000/Register/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'email': email, 'password': password}),
+      );
+
+      if (response.statusCode == 200) {
+        // Registration successful
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Registration successful')),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LogInScreen()),
+        );
+      } else {
+        // Registration failed
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Registration failed: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // Background image
           Positioned.fill(
             child: Image.asset(
               'assets/images/Background_LogIn.jpg',
               fit: BoxFit.cover,
             ),
           ),
-
           const Align(
             alignment: Alignment.topLeft,
             child: Padding(
               padding: EdgeInsets.fromLTRB(20, 100, 20, 0),
               child: Column(
-                // Menempatkan teks di sebelah kiri
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  // Teks "Welcome Back" dan "Sign in Again"
                   Text(
                     'Welcome \nUser!',
                     style: TextStyle(
@@ -49,7 +84,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                       color: Color.fromARGB(255, 250, 250, 250),
                     ),
                   ),
-                  SizedBox(height: 10), // Memberikan jarak antar teks
+                  SizedBox(height: 10),
                   Text(
                     'Sign up to join',
                     style: TextStyle(
@@ -62,129 +97,93 @@ class _RegisterScreenState extends State<RegisterScreen> {
               ),
             ),
           ),
-
           Align(
-              alignment: Alignment.bottomCenter,
-              // Container berwarna putih untuk form login
-              child: Container(
-                padding: const EdgeInsets.all(30),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                    topRight: Radius.circular(50),
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(30),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.65,
+              child: ListView(
+                children: [
+                  const HeaderText(),
+                  const SizedBox(height: 20),
+                  RegisterForm(
+                    usernameController: _usernameController,
+                    emailController: _emailController,
+                    passwordController: _passwordController,
                   ),
-                ),
-                height: MediaQuery.of(context).size.height * 0.52,
-                child: ListView(
-                  children: const [
-                    //Text Register
-                    HeaderText(),
-                    SizedBox(height: 20),
-
-                    //Kolom Input
-                    RegisterForm(),
-                    SizedBox(height: 30),
-
-                    //Tombol Register
-                    RegisterButton(),
-                    SizedBox(height: 30),
-
-                    //Text SignUp
-                    SignUpText(),
-                  ],
-                ),
-              )),
+                  const SizedBox(height: 30),
+                  RegisterButton(onRegister: _register),
+                  const SizedBox(height: 30),
+                  const SignUpText(),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class HeaderText extends StatelessWidget {
-  const HeaderText({super.key});
+class RegisterForm extends StatelessWidget {
+  final TextEditingController usernameController;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
 
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(left: 10),
-      child: Text(
-        'Register',
-        style: TextStyle(
-          fontFamily: 'Poppins',
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
-}
-
-class RegisterForm extends StatefulWidget {
-  const RegisterForm({super.key});
-
-  @override
-  _RegisterFormState createState() => _RegisterFormState();
-}
-
-class _RegisterFormState extends State<RegisterForm> {
-  bool _obscureText = true;
-
-  void _togglePasswordVisibility() {
-    setState(() {
-      _obscureText = !_obscureText;
-    });
-  }
+  const RegisterForm({
+    super.key,
+    required this.usernameController,
+    required this.emailController,
+    required this.passwordController,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextField(
+          controller: usernameController,
           decoration: InputDecoration(
-            labelText: 'Enter your first name',
+            labelText: 'Username',
             labelStyle: const TextStyle(fontFamily: 'Poppins', fontSize: 13),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(15),
-            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
           ),
         ),
         const SizedBox(height: 15),
         TextField(
+          controller: emailController,
           decoration: InputDecoration(
-            labelText: 'Email Address',
+            labelText: 'Email',
             labelStyle: const TextStyle(fontFamily: 'Poppins', fontSize: 13),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
           ),
         ),
         const SizedBox(height: 15),
         TextField(
-          obscureText: _obscureText,
+          controller: passwordController,
+          obscureText: true,
           decoration: InputDecoration(
-            labelText: 'Create Password',
+            labelText: 'Password',
             labelStyle: const TextStyle(fontFamily: 'Poppins', fontSize: 13),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            suffixIcon: IconButton(
-              icon: Icon(
-                _obscureText ? Icons.visibility : Icons.visibility_off,
-              ),
-              onPressed: _togglePasswordVisibility,
-            ),
+            border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
           ),
         ),
-        const SizedBox(height: 15),
       ],
     );
   }
 }
 
 class RegisterButton extends StatelessWidget {
-  const RegisterButton({super.key});
+  final VoidCallback onRegister;
+
+  const RegisterButton({super.key, required this.onRegister});
 
   @override
   Widget build(BuildContext context) {
@@ -192,25 +191,14 @@ class RegisterButton extends StatelessWidget {
       child: SizedBox(
         width: double.infinity,
         child: ElevatedButton(
-          onPressed: () {
-            // Add navigation or Register functionality here
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const LogInScreen()),
-            );
-          },
+          onPressed: onRegister,
           style: ElevatedButton.styleFrom(
             backgroundColor: const Color.fromRGBO(10, 66, 63, 1),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
           ),
           child: const Text(
-            'Register',
-            style: TextStyle(
-              fontFamily: 'Poppins',
-              color: Colors.white,
-            ),
+            "Register",
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ),
@@ -228,16 +216,11 @@ class SignUpText extends StatelessWidget {
       children: [
         const Text(
           "Already have an account? ",
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 13,
-            color: Colors.black,
-          ),
+          style: TextStyle(fontFamily: 'Poppins', fontSize: 13, color: Colors.black),
         ),
         GestureDetector(
           onTap: () {
-            // Navigasi ke halaman Sign Up di sini
-            Navigator.push(
+            Navigator.pushReplacement(
               context,
               MaterialPageRoute(builder: (context) => const LogInScreen()),
             );
